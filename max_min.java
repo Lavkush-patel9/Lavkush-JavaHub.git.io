5ba95100fdf28602bbbccb62c2b59d4eb3414a9d
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-03-04 23:11:08 +0530

    Update max_min.java

--- max_min.java
+++ max_min.java
@@ -4,8 +4,8 @@ public class max_min {
         int min = arr[0];
         int max = arr[0];
 
-        for (int i = 0; i< arr.length;i++)
-        { if(arr[i] < min){
+        for (int i = 1; iv< arr.length; i++) {
+        if(arr[i] < min){
             min = arr[i];
         } else if (arr[i] > max) {
              max = arr[i]; 
