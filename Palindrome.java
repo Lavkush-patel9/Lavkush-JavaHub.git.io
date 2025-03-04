a715fc9b18d9670b8ffed6d7f0aed0bff6b20832
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-03-04 22:22:44 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 3, 1}; // Input array
+        boolean isPalindrome = true;   // Flag to check palindrome
+
+        int i = 0, j = array.length - 1;
+
+        while (i < j) {
+            if (array[i] != array[j]) {
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The array is a palindrome.");
+        } else {
+            System.out.println("The array is NOT a palindrome.");
+        }
+    }
+}
