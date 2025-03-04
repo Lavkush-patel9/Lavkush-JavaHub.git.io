2c9f749c090f274e95597f001d5acc544db1bec8
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-03-04 23:48:31 +0530

    Update FirstOccurence.java

--- FirstOccurence.java
+++ FirstOccurence.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
-
 public class FirstOccurence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the target value = ");
-        int x = sc.nextInt();
-        int [] array = new int[5];
-        System.out.println("enter the element of array = ");
-        for(int i = 0; i < array.length;i++)
+        System.out.println("enter the size of array = ");
+        int size = sc.nextInt(); //take the input size of array
+        int [] array = new int[size]; // allocate the memory of this size
+        System.out.println("enter the element of the array = ");
+        for(int i=0; i<array.length; i++)
         {
-            array[i] = sc.nextInt();
+            array[i] = sc.nextInt(); // take input element of array
         }
-        for (int i = 0; i < array.length; i++)
-        {
-            if (array[i] == x )
-            {
-                System.out.println("the first occurence is =" +i);
+        System.out.println("enter the target value of x = ");
+        int x = sc.nextInt(); // take input of target value
+
+        boolean found = false;
+        for(int i = 0; i<array.length; i++)
+        { if(array[i] == x){
+                System.out.println("the first occurrences is = " +i);
+                found = true;
                 break;
             }
-
         }
+        if (!found){
+            System.out.println("this element is not found in array ");
+        }
+    sc.close();
     }
 }
