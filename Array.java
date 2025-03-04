d5e1fa8e5ade240ae1d12a8f29f39028cc0f432c
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-03-04 23:57:20 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // for input from user
-        int [] array = new int[5]; // allocate the memory
-        int sum = 0; // for
-        for(int i = 0; i < array.length; i++)
-        {
+        System.out.println("enter the size of array = ");
+        int size = sc.nextInt();
+        int [] array = new int[size]; // allocate the memory
+        int sum = 0; // for calculation the solution
+        System.out.println("enter the element of the array = ");
+        for(int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt(); // for input of element in array
-            sum = array[i]+sum; // calculate the sum
+            sum  += array[i]; // calculate the sum
+
         }
         System.out.println("the sum of array = " +sum); // for print the sum of array
         sc.close();
