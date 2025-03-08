f3b3d35be51246e9a4627eea9d276d2ea34bff1f
Lavkush Patel <166002653+Lavkush-patel9@users.noreply.github.com>
2025-03-08 18:28:38 +0530

    Add files via upload

---
+++ BinaryLastOccurrence.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BinaryLastOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //take input of size of the array
+        System.out.println("enter the size of array = ");
+        int size = sc.nextInt();
+        int [] arr = new int[size]; // array declaration and initialization of array
+        //take the input of array element and array must be sorted
+        System.out.print("enter the array element = ");
+        for(int i=0 ; i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        // take input target value
+        System.out.println("enter the target value = ");
+        int key = sc.nextInt();
+        int ans = LastOccurrences(arr, key, 0,arr.length-1);
+        System.out.println("the last occurrences is = " +ans);
+
+    }
+    public static int LastOccurrences(int []arr , int key , int start , int end)
+    {
+        int result = -1;
+        while (start <= end ) // its execute till that start is smaller and not equal end
+        {
+            int mid = start + (end-start) / 2; // find the mid-element in the array
+
+            if(arr[mid] == key) {
+                result = mid; // store the value of mid in new variable
+                start = mid+1; // for continue searching element for first occurrence
+            } else if (arr[mid] < key) { // its compare the mid or key value if mid is smaller than key value move the start pointer mid+1
+                start = mid + 1;
+            } else if (arr[mid]>key) { // its also move the end pointer mid-1
+                end = mid - 1;
+            }
+        }
+        return result;
+    }
+}
